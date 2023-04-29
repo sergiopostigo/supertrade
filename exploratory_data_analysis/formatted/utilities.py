e833afe47213278366f79115ba563011b249372a
Sergio Postigo <sergio.postigo@pucp.pe>
2023-04-29 17:46:31 +0200

    Granularities function corrected (match iso calendar), now focus in graphs

--- exploratory_data_analysis/formatted/utilities.py
+++ exploratory_data_analysis/formatted/utilities.py
@@ -42,7 +42,7 @@ def generate_time_granularities(start_date, end_date):
     time_granularities['day_in_quarter'] = day_in_quarter(start_date,end_date)
     time_granularities['day_in_month'] = time_granularities.index.day
     time_granularities['day_in_week'] = time_granularities.index.day_of_week + 1
-    time_granularities['week_in_year'] = week_in_year(start_date, end_date)
+    time_granularities['week_in_year'] = time_granularities.index.isocalendar().week
     time_granularities['week_in_semester'] = week_in_semester(start_date, end_date)
     time_granularities['week_in_quarter'] = week_in_quarter(start_date, end_date)
     time_granularities['week_in_month'] = week_in_month(start_date, end_date)
@@ -51,144 +51,174 @@ def generate_time_granularities(start_date, end_date):
 
     return time_granularities
 
-def week_in_year(start_date, end_date):
+## FOR DAYS COUNT GENERATIONS
 
-    # Create the dataframe that includes all the dates of the corresponding years
-    df = pd.date_range(start=start_date.replace(day=1, month=1), end=end_date.replace(day=31, month=12), freq='D')
-    df = pd.DataFrame(index=df)
-    # Define the needed
-    df['year'] = df.index.year
-    df['day_in_week'] = df.index.weekday + 1
+def is_first_day_of_semester(date):
+    if date.month == 1 and date.day == 1:
+        return 'first_day_of_period'
+    elif date.month == 7 and date.day == 1:
+        return 'first_day_of_period'
+    else: return 0
 
-    # Generate the counts
-    week_count = []
-    for year in df['year'].unique().tolist():
-        count = 1
-        for i in df[df['year'] == year]['day_in_week'].tolist():
-            if i == 7:
-                week_count.append(count)
-                count = count + 1
-            else:
-                week_count.append(count)
-    df['corrected_day_in_week'] = week_count
+def is_first_day_of_quarter(date):
+    if date.month == 1 and date.day == 1:
+        return 'first_day_of_period'
+    elif date.month == 4 and date.day == 1:
+        return 'first_day_of_period'
+    elif date.month == 7 and date.day == 1:
+        return 'first_day_of_period'
+    elif date.month == 10 and date.day == 1:
+        return 'first_day_of_period'
+    else: return 0
 
-    return df[df.index.isin(pd.date_range(start=start_date, end=end_date, freq='D'))]['corrected_day_in_week'].tolist()
-
-def week_in_month(start_date, end_date):
-
-    # Create the dataframe that includes all the dates of the corresponding years
-    df = pd.date_range(start=start_date.replace(day=1, month=1), end=end_date.replace(day=31, month=12), freq='D')
-    df = pd.DataFrame(index=df)
-    # Define the needed
-    df['year'] = df.index.year
-    df['month_in_year'] = df.index.month
-    df['day_in_week'] = df.index.weekday + 1
-
-    # Generate the counts
-    week_count = []
-    for year in df['year'].unique().tolist():
-        for month in df['month_in_year'].unique().tolist():
-            count = 1
-            for i in df[(df['year'] == year) & (df['month_in_year'] == month)]['day_in_week'].tolist():
-                if i == 7:
-                    week_count.append(count)
-                    count = count + 1
-                else:
-                    week_count.append(count)
-    df['week_in_month'] = week_count
-
-    return df[df.index.isin(pd.date_range(start=start_date, end=end_date, freq='D'))]['week_in_month'].tolist()
-
-def week_in_semester(start_date, end_date):
-
-    # Create the dataframe that includes all the dates of the corresponding years
-    df = pd.date_range(start=start_date.replace(day=1, month=1), end=end_date.replace(day=31, month=12), freq='D')
-    df = pd.DataFrame(index=df)
-    # Define the needed
-    df['year'] = df.index.year
-    df['semester_in_year'] = np.ceil(df.index.month / 6).astype(np.int64)
-    df['day_in_week'] = df.index.weekday + 1
-
-    # Generate the counts
-    week_count = []
-    for year in df['year'].unique().tolist():
-        for quarter in df['semester_in_year'].unique().tolist():
-            count = 1
-            for i in df[(df['year'] == year) & (df['semester_in_year'] == quarter)]['day_in_week'].tolist():
-                if i == 7:
-                    week_count.append(count)
-                    count = count + 1
-                else:
-                    week_count.append(count)
-    df['week_in_semester'] = week_count
-
-    return df[df.index.isin(pd.date_range(start=start_date, end=end_date, freq='D'))]['week_in_semester'].tolist()
-
-def week_in_quarter(start_date, end_date):
-
-    # Create the dataframe that includes all the dates of the corresponding years
-    df = pd.date_range(start=start_date.replace(day=1, month=1), end=end_date.replace(day=31, month=12), freq='D')
-    df = pd.DataFrame(index=df)
-    # Define the needed
-    df['year'] = df.index.year
-    df['quarter_in_year'] = np.ceil(df.index.month / 3).astype(np.int64)
-    df['day_in_week'] = df.index.weekday + 1
-
-    # Generate the counts
-    week_count = []
-    for year in df['year'].unique().tolist():
-        for quarter in df['quarter_in_year'].unique().tolist():
-            count = 1
-            for i in df[(df['year'] == year) & (df['quarter_in_year'] == quarter)]['day_in_week'].tolist():
-                if i == 7:
-                    week_count.append(count)
-                    count = count + 1
-                else:
-                    week_count.append(count)
-    df['week_in_quarter'] = week_count
-
-    return df[df.index.isin(pd.date_range(start=start_date, end=end_date, freq='D'))]['week_in_quarter'].tolist()
+def day_count_generator(lst):
+    new_lst = lst
+    day_count =  1
+    for i, val in enumerate(new_lst):
+        if val == 'first_day_of_period':
+            day_count = 1
+            new_lst[i] = day_count
+        else:
+            day_count = day_count + 1
+            new_lst[i] = day_count
+    return new_lst
 
 def day_in_semester(start_date, end_date):
 
-    # Create the dataframe that includes all the dates of the corresponding years
-    df = pd.date_range(start=start_date.replace(day=1, month=1), end=end_date.replace(day=31, month=12), freq='D')
+    # Create the dataframe with date index (extending the starting date to the first day of the corresponding year)
+    df = pd.date_range(start=start_date.replace(day=1, month=1), end=end_date, freq='D')
     df = pd.DataFrame(index=df)
-    # Define the needed
-    df['year'] = df.index.year
-    df['semester_in_year'] = np.ceil(df.index.month / 6).astype(np.int64)
-    df['day_in_year'] = df.index.day_of_year
 
-    # Generate the counts
-    day_count = []
-    for year in df['year'].unique().tolist():
-        for semester in df['semester_in_year'].unique().tolist():
-            count = 1
-            for i in df[(df['year'] == year) & (df['semester_in_year'] == semester)]['day_in_year'].tolist():
-                day_count.append(count)
-                count = count + 1
-    df['day_in_semester'] = day_count
+    # Generate the day count
+    df['day_in_semester'] = day_count_generator([is_first_day_of_semester(i) for i in df.index])
 
-    return df[df.index.isin(pd.date_range(start=start_date, end=end_date, freq='D'))]['day_in_semester'].tolist()
+    # Keep only dates from start_date to end_date
+    df = df.loc[start_date:end_date]
+
+    return df
 
 def day_in_quarter(start_date, end_date):
 
-    # Create the dataframe that includes all the dates of the corresponding years
-    df = pd.date_range(start=start_date.replace(day=1, month=1), end=end_date.replace(day=31, month=12), freq='D')
+    # Create the dataframe with date index (extending the starting date to the first day of the corresponding year)
+    df = pd.date_range(start=start_date.replace(day=1, month=1), end=end_date, freq='D')
     df = pd.DataFrame(index=df)
-    # Define the needed
-    df['year'] = df.index.year
-    df['quarter_in_year'] = np.ceil(df.index.month / 3).astype(np.int64)
-    df['day_in_year'] = df.index.day_of_year
 
-    # Generate the counts
-    day_count = []
-    for year in df['year'].unique().tolist():
-        for quarter in df['quarter_in_year'].unique().tolist():
-            count = 1
-            for i in df[(df['year'] == year) & (df['quarter_in_year'] == quarter)]['day_in_year'].tolist():
-                day_count.append(count)
-                count = count + 1
-    df['day_in_quarter'] = day_count
+    # Generate the day count
+    df['day_in_quarter'] = day_count_generator([is_first_day_of_quarter(i) for i in df.index])
 
-    return df[df.index.isin(pd.date_range(start=start_date, end=end_date, freq='D'))]['day_in_quarter'].tolist()
+    # Keep only dates from start_date to end_date
+    df = df.loc[start_date:end_date]
+
+    return df
+
+
+## FOR WEEKS COUNT GENERATIONS
+def is_first_thursday_of_month(date):
+    if date.weekday() == 3 and date.day <= 7:
+        return 'first_thursday'
+    else: return 0
+#%%
+def is_first_thursday_of_quarter(date):
+    if date.month ==1 and date.weekday() == 3 and date.day <= 7:
+        return 'first_thursday'
+    elif date.month ==4 and date.weekday() == 3 and date.day <= 7:
+        return 'first_thursday'
+    elif date.month ==7 and date.weekday() == 3 and date.day <= 7:
+        return 'first_thursday'
+    elif date.month ==10 and date.weekday() == 3 and date.day <= 7:
+        return 'first_thursday'
+    else: return 0
+#%%
+def is_first_thursday_of_semester(date):
+    if date.month ==1 and date.weekday() == 3 and date.day <= 7:
+        return 'first_thursday'
+    elif date.month ==7 and date.weekday() == 3 and date.day <= 7:
+        return 'first_thursday'
+    else: return 0
+
+def week_count_generator(lst):
+    new_lst = lst
+    # First fill the first weeks
+    for i, val in enumerate(lst):
+     if val=='first_thursday':
+        # Set first thursday as 1
+        new_lst[i]=1
+        # Set previous 3 days as 1
+        for j in range(max(0, i-3), i):
+            new_lst[j] = 1
+        # Set next 3 days as 1
+        for j in range(i+1, min(len(lst), i+4)):
+            new_lst[j] = 1
+    # Now fill the other weeks
+    for i, val in enumerate(new_lst):
+        if val == 1:
+            week = 2
+            weekday = 0
+            continue
+        else:
+            # Get current week
+            weekday = weekday + 1
+            if weekday < 7:
+                new_lst[i] = week
+            elif weekday == 7:
+                new_lst[i] = week
+                week = week + 1
+                weekday = 0
+    return new_lst
+
+def week_in_month(start_date, end_date):
+
+    # Get the most recent year whose first day was a thursday
+    corrected_start_date = start_date.replace(day=1, month=1)
+    while ((corrected_start_date.weekday() + 1) != 1):
+        corrected_start_date = corrected_start_date.replace(year=(corrected_start_date.year - 1))
+
+    # Dataframe with a date index in daily frequency
+    df = pd.date_range(start=corrected_start_date, end=end_date, freq='D')
+    df = pd.DataFrame(index=df)
+
+    # Generate the week count
+    df['week_in_month'] = week_count_generator([is_first_thursday_of_month(i) for i in df.index])
+
+    # Keep only dates from start_date to end_date
+    df = df.loc[start_date:end_date]
+
+    return df
+
+def week_in_semester(start_date, end_date):
+
+    # Get the most recent year whose first day was a thursday
+    corrected_start_date = start_date.replace(day=1, month=1)
+    while ((corrected_start_date.weekday() + 1) != 1):
+        corrected_start_date = corrected_start_date.replace(year=(corrected_start_date.year - 1))
+
+    # Dataframe with a date index in daily frequency
+    df = pd.date_range(start=corrected_start_date, end=end_date, freq='D')
+    df = pd.DataFrame(index=df)
+
+    # Generate the week count
+    df['week_in_semester'] = week_count_generator([is_first_thursday_of_semester(i) for i in df.index])
+
+    # Keep only dates from start_date to end_date
+    df = df.loc[start_date:end_date]
+
+    return df
+
+def week_in_quarter(start_date, end_date):
+
+    # Get the most recent year whose first day was a thursday
+    corrected_start_date = start_date.replace(day=1, month=1)
+    while ((corrected_start_date.weekday() + 1) != 1):
+        corrected_start_date = corrected_start_date.replace(year=(corrected_start_date.year - 1))
+
+    # Dataframe with a date index in daily frequency
+    df = pd.date_range(start=corrected_start_date, end=end_date, freq='D')
+    df = pd.DataFrame(index=df)
+
+    # Generate the week count
+    df['week_in_quarter'] = week_count_generator([is_first_thursday_of_quarter(i) for i in df.index])
+
+    # Keep only dates from start_date to end_date
+    df = df.loc[start_date:end_date]
+
+    return df
